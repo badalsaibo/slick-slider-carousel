6f93457a4cd8469c23bcb462f00ccb9dd91d3e06
Badal Raj Saibo <14058003+heyDante@users.noreply.github.com>
2021-07-07 13:30:19 +0530

    Push

---
+++ scripts/data.js
@@ -0,0 +1,6 @@
+const mediaContents = [
+  { fileName: 'img-1', type: 'image' },
+  { fileName: 'img-2', type: 'image' },
+  { fileName: 'img-3', type: 'image' },
+  { fileName: 'video.mp4', type: 'video' },
+];
